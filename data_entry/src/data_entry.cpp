134143ccce7ad8cf916271225084ac7b81d2c764
Daniel Palliser <danielpalliser@gmail.com>
2012-03-19 15:45:25 +0000

    made data_entry into a class

--- data_entry/src/data_entry.cpp
+++ data_entry/src/data_entry.cpp
@@ -6,31 +6,32 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <sstream>
 using namespace std;
 
-template<typename Type>
-void GetInput(Type &destination, std::string prompt);
-
-int main() {
+class data_entry{
+public:
+	template<typename Type>
+	void GetInput(Type &destination, std::string prompt);
+	void WriteShipFile();
+	void getInformation();
+protected:
 	int mmsi, course;
 	std::string response, name;
 	float lat, lng, speed;
-	std::cout << "Please enter information about a ship/n";
-	GetInput(mmsi, "MMSI: ");
-	GetInput(name, "Ship Name: ");
-	GetInput(lat, "Latitude(in the format (-)xx.xxx): ");
-	GetInput(lng, "Longitude (in the format (-)xx.xxx):");
-	GetInput(course, "Course made good: ");
-	GetInput(speed, "Speed over Ground (in the format xx.x): ");
+};
+
+data_entry::data_entry(){
+
 }
 
 /**
  * function gets input and checks type
  */
 template<typename Type>
-void GetInput(Type &destination, std::string prompt) {
+void data_entry::GetInput(Type &destination, std::string prompt) {
 	while (1) {
 
 		std::cout << prompt;
@@ -44,5 +45,25 @@ void GetInput(Type &destination, std::string prompt) {
 			break;
 		}
 	}
+}
+void data_entry::WriteShipFile(){
+	std::string filename;
+	filename  = mmsi;
+	ofstream file (filename);
+}
+
+void data_entry::getInformation(){
+	std::cout << "Please enter information about a ship/n";
+	GetInput(mmsi, "MMSI: ");
+	GetInput(name, "Ship Name: ");
+	GetInput(lat, "Latitude(in the format (-)xx.xxx): ");
+	GetInput(lng, "Longitude (in the format (-)xx.xxx):");
+	GetInput(course, "Course made good: ");
+	GetInput(speed, "Speed over Ground (in the format xx.x): ");
+}
+
+int main() {
+	data_entry dataentry;
+	dataentry.getInformation();
 }
 
