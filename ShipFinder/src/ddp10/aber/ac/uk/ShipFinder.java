0cdcfd0fd0fbfbaa2be078f20689e46635cc109c
Daniel Palliser <danielpalliser@gmail.com>
2012-03-21 12:31:13 +0000

    Started work on Java Ship Finder

---
+++ ShipFinder/src/ddp10/aber/ac/uk/ShipFinder.java
@@ -0,0 +1,17 @@
+package ddp10.aber.ac.uk;
+
+public class ShipFinder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ShipFinder finder = new ShipFinder();
+		finder.runShipFinding();
+	}
+
+	private void runShipFinding() {
+		
+	}
+
+}
