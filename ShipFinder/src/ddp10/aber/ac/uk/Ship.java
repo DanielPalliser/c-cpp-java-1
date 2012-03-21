7475a552d1d1fffafb0a7f4d5ffc6edc3d6bde4f
Daniel Palliser <danielpalliser@gmail.com>
2012-03-21 15:41:14 +0000

    ShipFinder first implementation, no file locking

---
+++ ShipFinder/src/ddp10/aber/ac/uk/Ship.java
@@ -0,0 +1,65 @@
+package ddp10.aber.ac.uk;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Ship {
+
+	private String mmsi;
+	private String name;
+	private Float lat;
+	private Float lng;
+	private Integer course;
+	private Float speed;
+
+	public Ship(String mmsi, String name, Float lat, Float lng, Integer course,
+			Float speed) {
+		this.mmsi = mmsi;
+		this.name = name;
+		this.lat = lat;
+		this.lng = lng;
+		this.course = course;
+		this.speed = speed;
+	}
+
+	protected Ship(File shipFile) throws IOException {
+		Boolean ret = true;
+		BufferedReader br = new BufferedReader(new FileReader(shipFile));
+		this.mmsi = br.readLine();
+		this.name = br.readLine();
+		this.lat = Float.valueOf(br.readLine());
+		this.lng = Float.valueOf(br.readLine());
+		this.course = Integer.valueOf(br.readLine());
+		this.speed = Float.valueOf(br.readLine());
+		br.close();
+	}
+
+	public String toString() {
+		StringBuffer ret = new StringBuffer();
+		ret.append(mmsi);
+		ret.append("\n");
+		ret.append(name);
+		ret.append("\n");
+		ret.append(lat);
+		ret.append("\n");
+		ret.append(lng);
+		ret.append("\n");
+		ret.append(course);
+		ret.append("\n");
+		ret.append(speed);
+		return ret.toString();
+	}
+
+	public Boolean matches(String searchTerm, int field) {
+		Boolean ret = false;
+		if (0 == field && name.contains(searchTerm)) {
+			ret = true;
+		} else if (1 == field && name.contains(searchTerm)){
+			ret = true;
+		}
+		return ret;
+	}
+}
--- ShipFinder/src/ddp10/aber/ac/uk/ShipFinder.java
+++ ShipFinder/src/ddp10/aber/ac/uk/ShipFinder.java
@@ -1,17 +1,102 @@
 package ddp10.aber.ac.uk;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Vector;
+
 public class ShipFinder {
 
 	/**
 	 * @param args
 	 */
+	private Scanner scan = new Scanner(System.in);
+
+	private void runShipFinding() {
+		String searchTerm;
+		while (true) {
+			System.out.println("Do you want to search for MMSI or ship Name?"
+					+ " type \"MMSI\" \"Name\" or \"q\" to quit");
+			String choice = scan.nextLine();
+			if (choice.equalsIgnoreCase("q")) {
+				break;
+			} else if (choice.equalsIgnoreCase("mmsi")) {
+				System.out.println("please enter the MMSI of the ship");
+				Integer searchField = new Integer(0);
+				search(searchField);
+			} else if (choice.equalsIgnoreCase("name")) {
+				System.out.println("please enter the name of the ship");
+				searchTerm = scan.nextLine();
+				Integer searchField = new Integer(0);
+				search(searchField);
+			}
+		}
+	}
+
+	private void search(Integer searchField) {
+		String searchTerm; // search for ships matching search term
+		searchTerm = scan.nextLine();
+		Vector<Ship> ships = searchWorkingDir(searchTerm, searchField);
+		for (Ship ship : ships) { // print out information about ships
+			System.out.println(ship.toString());
+		}
+		try { // write to log file
+			BufferedWriter bw = new BufferedWriter(new FileWriter(new File(
+					"log.txt")));
+			StringBuffer str = new StringBuffer(
+					"Search was made for ships with ");
+			if (0 == searchField) {
+				str.append("an MMSI ");
+			} else {
+				str.append("a name ");
+			}
+			str.append("matching ");
+			str.append(searchTerm);
+			bw.append(str.toString());
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param searchTerm
+	 * @param field
+	 *            0 for MMSI 1 for name
+	 * @return
+	 */
+	private Vector<Ship> searchWorkingDir(String searchTerm, int field) {
+		Vector<Ship> foundShips = new Vector<Ship>(10, 2); // vector to store
+															// results of search
+		File workingDir = new File(System.getProperty("user.dir"));
+		String[] children = workingDir.list();
+		for (String s : children) { // for each file
+			System.err.println(s);
+			if (s.matches("*.shp")) { // check file is a ship file
+				File shipFile = new File(s); // try to read in file
+				try {
+					Ship ship = new Ship(shipFile);
+					if (ship.matches(searchTerm, field)) {// save result if ship
+						foundShips.add(ship); // matches search term
+					}
+				} catch (FileNotFoundException e) {
+					System.err.println("filcheck e not found");
+				} catch (IOException e) {
+					System.err.println("error reading file");
+				}
+			}
+		}
+		return foundShips;
+	}
+
 	public static void main(String[] args) {
 		ShipFinder finder = new ShipFinder();
 		finder.runShipFinding();
-	}
-
-	private void runShipFinding() {
-		
 	}
 
 }
